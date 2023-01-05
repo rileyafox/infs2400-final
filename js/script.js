c512e34b1972957a79c9d0298933ae2e1f524b03
Riley <rileyfox134@gmail.com>
2023-01-04 18:18:54 -0600

    Initial file upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    $('#bar-1').jqbar({ label: 'ASP.NET', value: 99, barColor: '#D64747' });
+    $('#bar-2').jqbar({ label: 'C#', value: 99, barColor: '#FF681F' });
+    $('#bar-3').jqbar({ label: 'Javascript', value: 90, barColor: '#ea805c' });
+    $('#bar-4').jqbar({ label: 'HTML5', value: 50, barColor: '#88bbc8' });
+    $('#bar-5').jqbar({ label: 'CSS3', value: 60, barColor: '#939393' });
+    $('#bar-6').jqbar({ label: 'jQuery', value: 70, barColor: '#3a89c9' });
+    
+
+
+
+});
+
+
